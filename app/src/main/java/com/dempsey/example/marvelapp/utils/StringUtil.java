026aa16a198a4e49f2bff3eff04b7a5691cb0175
barryd <barry.dempsey@mttnow.com>
2017-11-24 00:42:46 +0000

    https://github.com/virtumedia/marvel
    * updated StringUtil modifiers

--- app/src/main/java/com/dempsey/example/marvelapp/utils/StringUtil.java
+++ app/src/main/java/com/dempsey/example/marvelapp/utils/StringUtil.java
@@ -10,14 +10,14 @@ public final class StringUtil {
     return string == null || string.length() == 0;
   }
 
-  public static String trimSpaces(String str) {
+  static String trimSpaces(String str) {
     if (!TextUtils.isEmpty(str)) {
       return str.replaceAll("\\s+", " ").trim();
     }
     return "";
   }
 
-  public static String getWordsFromString(final String arg) {
+  static String getWordsFromString(final String arg) {
     Pattern pattern = Pattern.compile("([\\S]+\\s*){1,4}");
     Matcher matcher = pattern.matcher(arg);
     matcher.find();
